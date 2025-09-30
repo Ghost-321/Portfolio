7392be21c71c5a0267cf4cf503be990fe61a5502
Ghost-321 <133848705+Ghost-321@users.noreply.github.com>
2025-09-30 18:20:56 +1000

    Update header.js

--- components/header.js
+++ components/header.js
@@ -7,40 +7,32 @@ class Header extends HTMLElement {
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-          <h1><a class="navbar-brand" href="Portfolio/index.html">Jonathan Morel</a></h1>
-          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="navbar-collapse collapse" id="navbarCollapse">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item mr-2">
-                <a class="nav-link" href="Portfolio/index.html">Home</a>
-              </li>
-              <li class="nav-item mr-2">
-                <a class="nav-link" href="Portfolio/pages/education.html">Education</a>
-              </li>
-              <li class="nav-item mr-2">
-                <a class="nav-link" href="Portfolio/pages/experience.html">Experience</a>
-              </li>
-              <li class="nav-item mr-2">
-                <a class="nav-link" href="Portfolio/pages/skills.html">Skills</a>
-              </li>
-              <li class="nav-item mr-2">
-                <a class="nav-link" href="Portfolio/pages/teaching.html">Teaching</a>
-              </li>
-              <li class="nav-item mr-2">
-                <a class="nav-link" href="Portfolio/pages/projects.html">Projects</a>
-              </li>
-              <!--
-              <li class="nav-item mr-2">
-                <a class="nav-link" href="Portfolio/pages/publications.html">Publications</a>
-              </li>
-              <li class="nav-item mr-2"> 
-                <a class="nav-link" href="Portfolio/pages/blogposts.html">Blogs</a>
-              </li>
-              -->
-            </ul>
-          </div>
+        <h1><a class="navbar-brand" href="/Portfolio/index.html">Jonathan Morel</a></h1>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="/Portfolio/index.html">Home</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="/Portfolio/pages/education.html">Education</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="/Portfolio/pages/experience.html">Experience</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="/Portfolio/pages/skills.html">Skills</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="/Portfolio/pages/teaching.html">Teaching</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="/Portfolio/pages/projects.html">Projects</a>
+            </li> 
+          </ul>
+        </div>
         </nav>
       </header>
     `;
